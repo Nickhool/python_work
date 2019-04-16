e601eea3db71dc4b149f8f0f31af43b3a659739e
NickSwag <1282622481@qq.com>
2019-04-16 16:26:57 +0800

    week2-ch8

---
+++ 8-函数/print_models.py
@@ -0,0 +1,18 @@
+__Author__ = "noduez"
+
+# 8-15 打印模型
+# 8-16 导入
+import print_functions as pf
+# import print_functions
+# from print_functions import print_models, show_completed_models
+# from print_functions import print_models as pm, show_completed_models as scm
+# from print_functions import *
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+pf.print_models(unprinted_designs, completed_models)
+pf.show_completed_models(completed_models)
+
+# print_models(unprinted_designs, completed_models)
+# show_completed_models(completed_models)
