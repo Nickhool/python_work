f0c8dc4d4f0a52ea4420c49fd8ae3df9f5859885
NickSwag <1282622481@qq.com>
2019-04-25 16:25:07 +0800

    0423-c16json文件

--- 16-下载数据/16-2_2.py
+++ 16-下载数据/16-2_2.py
@@ -1,1 +1,58 @@
 __Author__ = "noduez"
+
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+
+def get_weather_data(filename, dates, highs, lows):
+    """从数据文件中获取最高气温和最低气温。"""
+    with open(filename) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+
+        # dates, highs, lows = [], [], []
+        for row in reader:
+            try:
+                current_date = datetime.strptime(row[0], "%Y-%m-%d")
+                high = int(row[1])
+                low = int(row[3])
+            except ValueError:
+                print(current_date, 'missing data')
+            else:
+                dates.append(current_date)
+                highs.append(high)
+                lows.append(low)
+
+
+# 获取锡特卡的气温数据。
+dates, highs, lows = [], [], []
+get_weather_data('sitka_weather_2014.csv', dates, highs, lows)
+
+# 根据锡特卡的数据绘制图表。
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red', alpha=0.6)
+plt.plot(dates, lows, c='blue', alpha=0.6)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.15)
+
+# 获取死亡谷的数据。
+dates, highs, lows = [], [], []
+get_weather_data('death_valley_2014.csv', dates, highs, lows)
+
+# 将死亡谷的数据添加到当前图表中。
+plt.plot(dates, highs, c='red', alpha=0.3)
+plt.plot(dates, lows, c='blue', alpha=0.3)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.05)
+
+# 设置图表的格式。
+title = "Daily high and low temperatures - 2014"
+title += "\nSitka, AK and Death Valley, CA"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.ylim(10, 120)
+
+plt.show()
